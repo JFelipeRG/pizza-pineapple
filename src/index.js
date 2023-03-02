66845dda5f01def587e59d776ae1da0cbe5050d1
Juan Felipe Ramirez <juanfel2024@gmail.com>
2023-03-03 00:13:25 +0100

    Logica principal.
    Archivo que trabaja con los componentes y el dom para rotar las
    cartas y devolver el mensaje en caso de que haya ganado.

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import { fillCards } from "./module/fillCards";
+
+const neighbors = {
+  0: [0, 1, 3, 4],
+  1: [0, 1, 2, 3, 4, 5],
+  2: [1, 2, 4, 5],
+  3: [0, 1, 3, 4, 6, 7],
+  4: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+  5: [1, 2, 4, 5, 7, 8],
+  6: [3, 4, 6, 7],
+  7: [3, 4, 5, 6, 7, 8],
+  8: [4, 5, 7, 8],
+};
+
+(async () => {
+  await fillCards();
+
+  const randomPositions = [];
+
+  for (let i = 0; i < 10; i++) {
+    const randomNumber = Math.floor(Math.random() * 9);
+    randomPositions.push(randomNumber);
+  }
+
+  for (const position of randomPositions) {
+    rotateCards(neighbors[position]);
+  }
+
+  const cards = document.querySelectorAll("flip-card");
+
+  for (const card of cards) {
+    card.addEventListener("click", e => flipCard(e.target));
+  }
+
+  const flipCard = (card) => {
+    const cardPosition = card.classList[0];
+    let winner = false;
+
+    rotateCards(neighbors[cardPosition]);
+
+    const flipPosition = [];
+
+    for (const card of cards) {
+      flipPosition.push(card.returnFlipPosition());
+    }
+
+    switch (flipPosition[0]) {
+      case "front":
+        winner = !flipPosition.includes("back");
+        break;
+      case "back":
+        winner = !flipPosition.includes("front");
+        break;
+    }
+
+    if (winner) {
+      alert("Has ganado!!");
+    }
+  };
+})();
+
+const rotateCards = (arrayPositions) => {
+  const cards = document.querySelectorAll("flip-card");
+
+  for (const position of arrayPositions) {
+    cards[position].rotateCard();
+  }
+};
